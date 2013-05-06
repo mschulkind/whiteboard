b81ab2c1d22d05acd0ba995a539212e4495d7f1a
Matthew Schulkind <mschulkind@gmail.com>
2013-05-06 18:19:20 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whiteboard::Application.config.secret_token = '81d7784c9dcb5b615b12ac50c6a8976553dc1aef2f51fed48381027bb829671e3e1c7207835ddc1fec804fd449f298223942e391a768e0ebc200420c0835d0da'
